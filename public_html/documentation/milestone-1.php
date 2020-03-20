ca88494cd494fae7593f77c0b35b6281e8b6c2f5
Taylor Shepherd <tshepherd1@cnm.edu>
2020-03-19 21:34:30 -0600

    finished milestone phase 1

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset= "UTF-8">
+	<title>My Personal Web Page</title>
+</head>
+
+<body>
+<p>
+	GOAL:
+</p>
+	The purpose of my personal web page project is to create a well rounded, well executed web page that I can eventually share with potential future employers to demonstrate my knowledge and skills relating to coding and full stack web development. My main objective of this project is to acquire a full or part-time employment or an internship.
+<p>
+	PURPOSE:
+</p>
+<p>
+	The purpose of this particular website is to educate people on as much information pertaining to Shar-Pei's as possible.
+</p>
+<p>
+	AUDIENCE:
+</p>
+<p>
+	This website is designed for not only people who LOVE Shar-Pei's, but also for people who own a Shar-Pei and are wanting to extend their knowledge and their ability to properly care for it. This website is also designed to educated people who might be looking into possibly adopting a Shar-Pei and to find out if a furry friend like a Shar-Pei would be a good match for them.
+</p>
+<p>
+	PERSONA:
+</p>
+<p>
+	Lindsey Smith
+</p>
+<p>
+	"Dog's are my favorite animal and I've always wanted one. But which one is right for me?" - Lindsey Smith
+</p>
+<p>
+	<ul>
+			<li>Name: Lindsey Smith</li>
+			<li>Age: 30 years old</li>
+			<li>From: Albuquerque, New Mexico</li>
+			<li>Job: Nurse</li>
+			<li>Marital Status: Single</li>
+			<li>Hobbies/Interests: Hiking, walks along the Rio Grande, cuddling up and watching movies on the weekends</li>
+</ul>
+</p>
+<p>
+	Lindsey Smith is a 30 year old, single female. She just recently finished her nursing program and was hired at the local hospital. She spends most of her time at work, seeing she works 12 hour days, 4 days a week. But when she is not at work, she is either watching movies at home or spending time outdoors. She spends most of her time outside of work alone and is looking to expand her home life by getting a dog to keep her company. She thinks she is interested in a Shar-Pei, purely based on their adorable looks, but wants to do more research on them to find out if a Shar-Pei would be a good fit for her and her lifestyle. Although dogs are her favorite animal, she has actually never owned one so she wants to do as much research she can to educate herself so she can go above and beyond caring for her dog. She uses her iPhone 10s while she is on the go, but when she is at home she mostly uses her Dell laptop; where she will most likely being doing the majority of her research.
+</p>
+<p>
+	NEEDS:
+</p>
+<p>
+	<ul>
+			<li>Is looking for a website that give her all the information she might need in picking the right kind of dog for herself</li>
+			<li>She wants to know what are the responsibilites that come along with owning a Shar-Pei</li>
+			<li>She wants to know what kind of lifestyle fits best for a Shar-Pei and if hers is compatible with one or not</li>
+			<li>Wants to know about health complications with Shar-Pei's (if any)</li>
+			<li>She wants to know the personality traits of most Shar-Pei's</li>
+			<li>She would like to learn tips and pointers when it comes to best caring for a Shar-Pei such as grooming, crate training, house training... etc</li>
+			<li>She would like to be able to read personal stories from actual Shar-Pei owners</li>
+</ul>
+</p>
+<p>
+	GOALS:
+</p>
+<p>
+	Lindsey feels as though she has reached the perfect stage in her life where she will be able to properly care for a dog and is wanting to find the perfect one for her and her lifestyle. Her goal is to do as much research as she can on Shar-Pei's, as that seems to be the dog she is most interested in. She is hoping to learn about how well a Shar-Pei would do being home alone for long periods of time while she is at work, if Albuquerque's climate is a good match for a Shar-Pei, if a Shar-Pei can keep up with her hikes or her long walks on the Rio Grande, the grooming and upkeep of the Shar Pei and how well most Shar-Pei's interact with other dogs, people and children. Her goal is to see if a Shar-Pei is the perfect dog for her.
+</p>
+<p>
+	USER STORY:
+</p>
+<p>
+	As a user, Lindsey Smith needs a Shar-Pei related website to be able to research any and all of her questions she may have surrounding Shar-Pei's to see if that specific breed is the perfect match for her or not.
+</p>
+</body>
+</html>
